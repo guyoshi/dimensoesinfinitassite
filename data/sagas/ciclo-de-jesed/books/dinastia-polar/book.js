d31270d743f31de9092ef6fca687f0e70e1a55bd
guyoshi <guilherme_dosanjos@hotmail.com>
2026-09-16 23:22:59 +0100

    Atualiza contador de capítulos de Dinastia Polar

--- data/sagas/ciclo-de-jesed/books/dinastia-polar/book.js
+++ data/sagas/ciclo-de-jesed/books/dinastia-polar/book.js
@@ -39,7 +39,7 @@
     },
     metadata:{
       dataDirectory:'data/sagas/ciclo-de-jesed/books/dinastia-polar',optionalCollections:['themes','mysteries'],
-      legacySources:['data/sagas/ciclo-de-jesed/books/dinastia-polar/runtime.js'],stage:'Em desenvolvimento · 19 capítulos'
+      legacySources:['data/sagas/ciclo-de-jesed/books/dinastia-polar/runtime.js'],stage:'Em desenvolvimento · 23 capítulos'
     }
   });
 })();
